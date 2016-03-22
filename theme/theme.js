b71694ae17aa76c5092516fac0a7d778ec9c3461
tovanda <amndrva@gmail.com>
2016-03-22 16:35:18 +0200

    Added files via upload

---
+++ theme/theme.js
@@ -0,0 +1,9 @@
+var theme =
+[
+	['<IMG SRC=theme/folder.gif> ', '<IMG SRC=theme/folderopen.gif> '],
+	['', ''],
+	['', ''],
+	'<IMG SRC=theme/page.gif> ',
+	['', ''],
+	['&nbsp;&nbsp;&nbsp;&nbsp;', '&nbsp;&nbsp;&nbsp;&nbsp;'],
+];
